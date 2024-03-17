61a44d773d14164f39fb424a31980e5c8651e3e9
Yonatan Dvir <64538919+yonatan-dvir@users.noreply.github.com>
2024-03-17 23:37:18 +0200

    Added functionallity to the Login/SignUp page - now we can switch between login to signup

--- src/components/LoginSignup/LoginSignup.tsx
+++ src/components/LoginSignup/LoginSignup.tsx
@@ -1,34 +1,41 @@
-import React from 'react';
+import React, { useState } from 'react';
 import './LoginSignup.css';
+
+import email_icon from "../assets/email-icon.png";
+import password_icon from "../assets/padlock.png";
+import user_icon from "../assets/user.png";
 
 
 const LoginSignup = () => {
+
+  const [action,setAction] = useState("Sign Up");
+
   return (
   <div className='container'>
 
       <div className='header'>
-        <div className='text'>Sign Up</div>
+        <div className='text'>{action}</div>
         <div className='underline'></div>
       </div>
 
       <div className='inputs'>
       <div className='input'>
-          <img src='' alt=''></img>
+          <img src={user_icon} alt=''></img>
           <input type='text' placeholder='Name'/>
       </div>
       <div className='input'>
-          <img src='' alt=''></img>
+          <img src={email_icon} alt=''></img>
           <input type='email' placeholder='Email Id'/>
       </div>
       <div className='input'>
-          <img src='' alt=''></img>
+          <img src={password_icon} alt=''></img>
           <input type='password' placeholder='Password'/>
       </div>
       </div>
-      <div className='forgot-password'>Losr Password? <span>Click here!</span></div>
+      <div className='forgot-password'>Lost Password? <span>Click here!</span></div>
       <div className='submit-container'>
-        <div className='submit'>Sign Up</div>
-        <div className='submit'>Login</div>
+        <div className={action=='Sign Up'?'submit':'submit gray'} onClick={()=>setAction('Sign Up')}>Sign Up</div>
+        <div className={action=='Login'?'submit':'submit gray'} onClick={()=>setAction('Login')}>Login</div>
       </div>
   </div>
   )
